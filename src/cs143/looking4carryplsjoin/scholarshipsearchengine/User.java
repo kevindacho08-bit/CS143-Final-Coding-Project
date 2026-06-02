e237aa383ee6a04c5ff4fc247c0d5c13c942d4a6
dxvu4-dev <dxvu4@students.everettcc.edu>
2026-06-02 10:56:45 -0700

    Update User.java

--- src/cs143/looking4carryplsjoin/scholarshipsearchengine/User.java
+++ src/cs143/looking4carryplsjoin/scholarshipsearchengine/User.java
@@ -3,6 +3,14 @@ package cs143.looking4carryplsjoin.scholarshipsearchengine;
 
 
 public class User extends Scholarship {
+	//default constructor
+	User() {
+		super();
+	}
 	
+	//parameterized constructor
+	User(String name, double GPA, String race, String gender, double income, String major, boolean isFirstGenImmigrant, boolean isWashingtonResident, double scholarshipAmount) {
+		super(name, GPA, race, gender, income, major, isFirstGenImmigrant, isWashingtonResident, scholarshipAmount);
+	}
 	
 }
