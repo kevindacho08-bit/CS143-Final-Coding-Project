ffcf2df39c41a55bf5ff9b10b9f65770e762219c
dxvu4-dev <dxvu4@students.everettcc.edu>
2026-06-02 10:17:02 -0700

    Update Scholarship.java

--- src/cs143/looking4carryplsjoin/scholarshipsearchengine/Scholarship.java
+++ src/cs143/looking4carryplsjoin/scholarshipsearchengine/Scholarship.java
@@ -27,23 +27,57 @@ public abstract class Scholarship {
 	public void setGPA(double GPA) {
 		this.GPA = GPA;
 	}
+	
 	public void setRace(String race) {
 		this.race = race;
 	}
+	
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	
 	public void setIncome(double income) {
 		this.income = income;
 	}
+	
 	public void setMajor(String major) {
 		this.major = major;
 	}
+	
 	public void setIsFirstGenImmigrant(boolean isFirstGenImmigrant) {
 		this.isFirstGenImmigrant = isFirstGenImmigrant;
 	}
+	
 	public void setIsWashingtonResident(boolean isWashingtonResident) {
 		this.isWashingtonResident = isWashingtonResident;
 	}
+	
 	//getter methods
+	public double getGPA() {
+		return GPA;
+	}
+	
+	public String getRace() {
+		return race;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public double getIncome() {
+		return income;
+	}
+	
+	public String getMajor() {
+		return major;
+	}
+	
+	public boolean getIsFirstGenImmigrant() {
+		return isFirstGenImmigrant;
+	}
+	
+	public boolean getIsWashingtonResident() {
+		return isWashingtonResident;
+	}
 }
