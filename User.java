74ae5a229563f304c253590c16888d8f0c73ff18
dxvu4-dev <dxvu4@students.everettcc.edu>
2026-06-04 11:40:25 -0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,21 @@
+
+package cs143.looking4carryplsjoin.scholarshipsearchengine;
+
+
+public class User extends Scholarship{
+	//default constructor
+	User() {
+		super();
+	}
+	
+	//parameterized constructor
+	User(String name, double GPA, String race, String gender, double income, String major, boolean isFirstGenImmigrant, boolean isWashingtonResident, double scholarshipAmount) {
+		super(name, GPA, race, gender, income, major, isFirstGenImmigrant, isWashingtonResident, scholarshipAmount);
+	}
+
+	@Override
+	public void printDescription() {
+		//System.out.println(this.getName() + " | " + this.getRace() + " | " + this.getGender());
+		// for david
+	}
+}
