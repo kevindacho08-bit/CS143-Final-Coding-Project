99c5509d4e36128de0ac2df258f9b3f8175e25b7
kevindacho08-bit <kevin.dac.ho.08@gmail.com>
2026-06-09 00:23:34 -0700

    Update MicrosoftScholarship.java
    MicorsoftScholarship done

--- src/cs143/looking4carryplsjoin/scholarshipsearchengine/MicrosoftScholarship.java
+++ src/cs143/looking4carryplsjoin/scholarshipsearchengine/MicrosoftScholarship.java
@@ -2,13 +2,16 @@ package cs143.looking4carryplsjoin.scholarshipsearchengine;
 
 public class MicrosoftScholarship extends Scholarship{
 		MicrosoftScholarship() {
-			super(/**We can hard code the scholarship data here**/);
-			//this for u kevin
+			super("MicrosoftScholarship", 3.0, "Any", "Any", 89369, "Stem", true, true, 5000);
+		            
 		}
 
 		@Override
 		public void printDescription() {
 			// TODO Auto-generated method stub
-			
+			System.out.println("Microsoft Scholarship");
+	        System.out.println("Minimum GPA: 3.0");
+	        System.out.println("Major: Stem");
+	        System.out.println("Award Amount: $5000");
 		}
 }
