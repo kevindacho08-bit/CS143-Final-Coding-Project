a070cf7f32aa925fbab7f83c91b745b30d79e88f
caseylabanimations <60805378+caseylabanimations@users.noreply.github.com>
2026-06-09 10:08:52 -0700

    Update CompareScholarships.java
    added returnEligible method

--- src/cs143/looking4carryplsjoin/scholarshipsearchengine/CompareScholarships.java
+++ src/cs143/looking4carryplsjoin/scholarshipsearchengine/CompareScholarships.java
@@ -9,18 +9,37 @@ public class CompareScholarships {
 		scholarships.add(new MicrosoftScholarship()); //option 2: use private method to fill arraylist
 	}
 	
-	//have method that displays filled arraylist
-	
-	//For Casey
-	/**public static ArrayList<Scholarship> returnEligible(User user) {
-	    ArrayList<Scholarship> elegibleScholarships = scholarships;
-		fillScholarships();
-
-		
-		Not sure how to make logic, maybe use nested loops or recurssion
-		
-		//we can have "Any" fields that ignore and just automatically accetp the comparison
-		
-		return scholarships;
-	}**/
+    //Remove scholarship from list if:
+    //Race, Gender, Major are not "Any" and do not match user inputs
+    //if user gpa less than scholarship minimum gpa
+    //if user income greater than maximum income for scholarship
+    //if user is not WA resident and scholarship requires WA residence
+    //if user is first gen and scholarship requires not first gen
+    public ArrayList<Scholarship> returnEligible(User user) {
+        for (Scholarship ss : scholarships) {
+            if (user.getGPA() <= ss.getGPA()) {
+                scholarships.remove(ss);
+            }
+            if (user.getIncome() >= ss.getIncome()) {
+                scholarships.remove(ss);
+            }
+            if ((!ss.getRace().toLowerCase().equals("any")) && (!ss.getRace().toLowerCase().equals(user.getRace()))) {
+                scholarships.remove(ss);
+            }
+            if ((!ss.getGender().toLowerCase().equals("any")) && (!ss.getGender().toLowerCase().equals(user.getGender()))) {
+                scholarships.remove(ss);
+            }
+            if ((!ss.getMajor().toLowerCase().equals("any")) && (!ss.getMajor().toLowerCase().equals(user.getMajor()))) {
+                scholarships.remove(ss);
+            }
+            if ((user.getIsWashingtonResident() == false) && (ss.getIsWashingtonResident() == true)) {
+                scholarships.remove(ss);
+            }
+            if ((user.getIsFirstGenImmigrant() == true) && (ss.getIsFirstGenImmigrant() == false)) {
+                scholarships.remove(ss);
+            }
+        }
+        
+        return scholarships;
+    }
 }
